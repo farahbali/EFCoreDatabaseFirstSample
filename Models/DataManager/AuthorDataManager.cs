2870a1c8c37a8d8e66f67620908631adb26fe431
Farah Bali <balifarah2001@gmail.com>
2025-03-07 21:47:56 +0100

    project setup

---
+++ Models/DataManager/AuthorDataManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EFCoreDatabaseFirstSample.Models.DTO;
+using EFCoreDatabaseFirstSample.Models.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreDatabaseFirstSample.Models.DataManager
+{
+    public class AuthorDataManager : IDataRepository<Author, AuthorDto>
+    {
+        readonly BookStoreContext _bookStoreContext;
+
+        public AuthorDataManager(BookStoreContext storeContext)
+        {
+            _bookStoreContext = storeContext;
+        }
+
+        public void Add(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAll()
+        {
+            return _bookStoreContext.Author
+                .Include(author => author.AuthorContact)
+                .ToList();
+        }
+
+        public AuthorDto GetDto(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Author entityToUpdate, Author entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using EFCoreDatabaseFirstSample.Models.DataManager;
+using EFCoreDatabaseFirstSample.Models.DTO;
+using EFCoreDatabaseFirstSample.Models.Repository;
+using EFCoreDatabaseFirstSample.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EFCoreDatabaseFirstSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BookStoreContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:BooksDB"]));
+            services.AddScoped<IDataRepository<Author, AuthorDto>, AuthorDataManager>();
+            services.AddScoped<IDataRepository<Book, BookDto>, BookDataManager>();
+            services.AddScoped<IDataRepository<Publisher, PublisherDto>, PublisherDataManager>();
+
+            services.AddControllers()
+                .AddNewtonsoftJson(
+                    options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
